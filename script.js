9aca1eece3b2ecd0144d07a12a4a9a8b85e5f014
miminaki <dimitramaz@yahoo.gr>
2018-04-03 01:48:34 +0300

    changed active state + if statement

--- script.js
+++ script.js
@@ -1,4 +1,26 @@
 // $("button#btn1").click(function(){
 // 	var navItem = $(this).
 // });
-$("button#btn1").click(toggleClass('active'));
+$(document).ready(function(){
+	$("#btn-1").click(function(){
+		$(this).toggleClass("toggled active");
+	});
+	$("#btn-2").click(function(){
+		$(this).toggleClass("toggled active");
+	});
+	$("#btn-3").click(function(){
+		$(this).toggleClass("toggled active");
+	});
+
+	// ifs
+	if ($('#btn-1').hasClass('active')){
+
+	}
+});
+
+if (true) {}
+
+// $( "#btn-1" ).click(function() {
+//   $( this ).toggleClass( "active" );
+// });
+
