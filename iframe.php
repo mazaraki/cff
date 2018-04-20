88ec809d52b0bd82546a6d849f6596e55fb6409e
Dimitra Mazaraki <miminaki@users.noreply.github.com>
2018-04-20 15:45:04 +0300

    20/4/2018

---
+++ iframe.php
@@ -0,0 +1,11 @@
+<div class="loading-info">
+	<div id="loading-info" class="loading-info-content">
+		<i class="fa fa-3x fa-spinner fa-pulse"></i>
+	</div>
+</div>
+
+<script language="javascript">
+	setTimeOut(function(){
+		document.getElementById('loading-info').innerHTML = "<p>USE LINK BELOW IFRAME</p>"
+	}, 5000);
+</script>
